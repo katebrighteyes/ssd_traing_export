2efad9e8a8adf1c0e968ebbc7084e84b128f8c02
Kate <49902595+katebrighteyes@users.noreply.github.com>
2020-01-20 17:06:03 +0900

    Update train_ex.sh

--- train_ex.sh
+++ train_ex.sh
@@ -1,6 +1,9 @@
+
 mkdir ~/tf_ssd/tod/save_models/
 
 mkdir ~/tf_ssd/tod/save_models/coco_test
+
+cd ~/tf_ssd/tod/train_models/research
 
 PIPELINE_CONFIG_PATH='/home/opencv-mds/tf_ssd/tod/train_models/research/object_detection/samples/configs/ssd_inception_v2_coco.config'
 
@@ -21,3 +24,5 @@ SAMPLE_1_OF_N_EVAL_EXAMPLES=1
 export TF_CPP_MIN_LOG_LEVEL=2
 
 python object_detection/model_main.py --pipeline_config_path=${PIPELINE_CONFIG_PATH} --model_dir=${MODEL_DIR} --num_train_steps=${NUM_TRAIN_STEPS} --num_eval_steps=${NUM_EVAL_STEPS} --checkpoint_dir=${PRE_TRAIN} --sample_1_of_n_eval_examples=$SAMPLE_1_OF_N_EVAL_EXAMPLES --num_clones=1 --ps_tasks=1
+
+
