096f733153b5d5c3f96e99fa2368d600f07430a2
Kate <49902595+katebrighteyes@users.noreply.github.com>
2020-08-02 22:00:46 +0900

    Update export_ex.sh

--- export_ex.sh
+++ export_ex.sh
@@ -7,3 +7,5 @@ TRAINED_CKPT_PREFIX='/tf_ssd/tod/save_models/coco_test/model.ckpt-20'
 EXPORT_DIR='/tf_ssd/tod/pbfiles'
 
 python object_detection/export_inference_graph.py --input_type=${INPUT_TYPE} --pipeline_config_path=${PIPELINE_CONFIG_PATH} --trained_checkpoint_prefix=${TRAINED_CKPT_PREFIX} --output_directory=${EXPORT_DIR}
+
+
