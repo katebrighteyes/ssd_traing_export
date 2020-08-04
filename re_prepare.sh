951da4fc8bb30d4d53c21d70c6f41be4ebf3c7e9
Kate <49902595+katebrighteyes@users.noreply.github.com>
2020-08-04 16:24:13 +0900

    Update re_prepare.sh

--- re_prepare.sh
+++ re_prepare.sh
@@ -1,15 +1,16 @@
 #****vBox 에서 다시 트레이닝 시작할때***
 
-cd ~/tf_ssd/
+cd /tf_ssd
 
 # 1. start venv
 source ./venvssd/bin/activate
 
+
 # 2. 파이썬 환경 세팅
-export PYTHONPATH=$PYTHONPATH:/home/opencv-mds/tf_ssd/tod/train_models/research:/home/opencv-mds/tf_ssd/tod/train_models/research/slim
+export PYTHONPATH=$PYTHONPATH:/tf_ssd/tod/train_models/research:/tf_ssd/tod/train_models/research/slim
 
 #3. 슬림 환경 세팅
-cd /home/opencv-mds/tf_ssd/tod/train_models/research
+cd /tf_ssd/tod/train_models/research
 protoc object_detection/protos/*.proto --python_out=.
 python object_detection/builders/model_builder_test.py
 
